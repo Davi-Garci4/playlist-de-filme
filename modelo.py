b044959311029a94e027ff1e064180efeefe844a
Davi-Garci4 <davi.garcia@aluno.faculdadeimpacta.com.br>
2023-08-05 13:26:29 -0300

    Criação das classes

---
+++ modelo.py
@@ -0,0 +1,19 @@
+class Filme:
+    def __init__(self, nome, ano, duracao,):
+        self.nome = nome.title()
+        self.ano = ano
+        self.duracao = duracao
+        self.likes = 0
+
+    def dar_likes(self):
+        self.likes += 1
+
+class Serie:
+    def __init__(self, nome, ano, temporada):
+        self.nome = nome.title()
+        self.ano = ano
+        self.temporada = temporada
+        self.likes = 0
+
+    def dar_likes(self):
+            self.likes += 1
