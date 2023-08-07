29559e8649b1f9281db0e1ad38823d2781513062
Davi-Garci4 <davi.garcia@aluno.faculdadeimpacta.com.br>
2023-08-07 13:05:16 -0300

    Adicionei algumas propriedades e métodos

--- modelo.py
+++ modelo.py
@@ -1,19 +1,45 @@
 class Filme:
     def __init__(self, nome, ano, duracao,):
-        self.nome = nome.title()
+        self.__nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.likes = 0
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
 
     def dar_likes(self):
-        self.likes += 1
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+
 
 class Serie:
     def __init__(self, nome, ano, temporada):
-        self.nome = nome.title()
+        self.__nome = nome.title()
         self.ano = ano
         self.temporada = temporada
-        self.likes = 0
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
 
     def dar_likes(self):
-            self.likes += 1
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
