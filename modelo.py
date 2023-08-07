85d9d055f3ed0b8397e8c9342caa198c330c6046
Davi-Garci4 <davi.garcia@aluno.faculdadeimpacta.com.br>
2023-08-07 14:58:36 -0300

    Adicionei a função __str__, para retornar os objetos em forma de string

--- modelo.py
+++ modelo.py
@@ -1,23 +1,27 @@
 class Programa:           # Essa é a classe mãe (herança) do filme e serie.
     def __init__(self, nome, ano):   #através do init, que é uma função construtora adicionei os atributos "nome e ano"
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
-        self.__likes = 0
+        self._likes = 0
 
     @property                # Aqui eu utilizei o property para criar o retorno de likes
     def likes(self):
-        return self.__likes
+        return self._likes
 
     def dar_likes(self):    #aqui é uma funçaõ de dar likes em um filme ou serie
-        self.__likes += 1
+        self._likes += 1
 
     @property            #Aqui eu retorno os nomes
     def nome(self):
-        return self.__nome
+        return self._nome
 
     @nome.setter          #Aqui eu mudo os nomes
     def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
+        self._nome = novo_nome.title()
+
+    def __str__(self):      # Aqui eu estou utilizando a função "__str__" para me retornar o objeto em forma de string
+        return f" {self._nome} - {self.ano} - {self._likes} Likes"
+
 
 
 class Filme (Programa):      #Criação da classe filme, entre parênteses esta a classe mãe utilizada como base
@@ -25,8 +29,16 @@ class Filme (Programa):      #Criação da classe filme, entre parênteses esta 
         super().__init__(nome, ano)     # Aqui eu utilizo o "super()" para chamar a classe mãe e o "__init__", para utilizar os atributos da classe mãe
         self.duracao = duracao
 
+    def __str__(self):
+        return f" {self._nome} - {self.ano} - {self.duracao} minutos -  {self._likes} Likes"
+
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporada):
         super().__init__(nome, ano)
         self.temporada = temporada
+
+    def __str__(self):
+        return f" {self._nome} - {self.ano} - {self.temporada} Temporadas -  {self._likes} Likes"
+
+
